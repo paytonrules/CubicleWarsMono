193085820e328cbbc69a4c480f27b9a3f89648a1
paytonrules <esmith@8thlight.com>
2013-04-20 22:03:52 -0500

    First pass with some new models

---
+++ CubicleWars/Game.cs
@@ -0,0 +1,132 @@
+#region File Description
+//-----------------------------------------------------------------------------
+// CubicleWarsGame.cs
+//
+// Microsoft XNA Community Game Platform
+// Copyright (C) Microsoft Corporation. All rights reserved.
+//-----------------------------------------------------------------------------
+#endregion
+
+#region Using Statements
+using System;
+
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Audio;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework.Input.Touch;
+using Microsoft.Xna.Framework.Storage;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Media;
+
+#endregion
+
+namespace CubicleWars
+{
+	/// <summary>
+	/// Default Project Template
+	/// </summary>
+	public class Game1 : Game
+	{
+
+	#region Fields
+		GraphicsDeviceManager graphics;
+		SpriteBatch spriteBatch;
+		Texture2D logoTexture;
+		Model model;
+		private Matrix world = Matrix.CreateTranslation(new Vector3(0, 0, 0));
+		private Matrix view = Matrix.CreateLookAt(new Vector3(0, 0, 10), new Vector3(0, 0, 0), Vector3.UnitY);
+		private Matrix projection = Matrix.CreatePerspectiveFieldOfView(MathHelper.ToRadians(45), 800f / 480f, 0.1f, 100f);
+	#endregion
+
+	#region Initialization
+
+		public Game1()
+		{
+			graphics = new GraphicsDeviceManager (this);
+			
+			Content.RootDirectory = "Content";
+
+			graphics.IsFullScreen = false;
+		}
+
+		/// <summary>
+		/// Overridden from the base Game.Initialize. Once the GraphicsDevice is setup,
+		/// we'll use the viewport to initialize some values.
+		/// </summary>
+		protected override void Initialize ()
+		{
+			base.Initialize ();
+		}
+
+
+		/// <summary>
+		/// Load your graphics content.
+		/// </summary>
+		protected override void LoadContent ()
+		{
+			// Create a new SpriteBatch, which can be use to draw textures.
+			spriteBatch = new SpriteBatch (graphics.GraphicsDevice);
+			
+			// TODO: use this.Content to load your game content here eg.
+			logoTexture = Content.Load<Texture2D> ("logo");
+
+			model = Content.Load<Model>("stapler");
+		}
+
+	#endregion
+
+	#region Update and Draw
+
+		/// <summary>
+		/// Allows the game to run logic such as updating the world,
+		/// checking for collisions, gathering input, and playing audio.
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Update (GameTime gameTime)
+		{
+			// TODO: Add your update logic here			
+            		
+			base.Update (gameTime);
+		}
+
+		/// <summary>
+		/// This is called when the game should draw itself. 
+		/// </summary>
+		/// <param name="gameTime">Provides a snapshot of timing values.</param>
+		protected override void Draw (GameTime gameTime)
+		{
+			// Clear the backbuffer
+			graphics.GraphicsDevice.Clear (Color.CornflowerBlue);
+
+			spriteBatch.Begin ();
+
+			// draw the logo
+			spriteBatch.Draw (logoTexture, new Vector2 (130, 200), Color.White);
+
+			spriteBatch.End();
+
+			DrawModel(model, world, view, projection);
+
+			//TODO: Add your drawing code here
+			base.Draw (gameTime);
+		}
+
+		private void DrawModel(Model model, Matrix world, Matrix view, Matrix projection)
+		{
+			foreach (ModelMesh mesh in model.Meshes)
+			{
+				foreach (BasicEffect effect in mesh.Effects)
+				{
+					effect.World = world;
+					effect.View = view;
+					effect.Projection = projection;
+				}
+				
+				mesh.Draw();
+			}
+		}
+
+	#endregion
+	}
+}
